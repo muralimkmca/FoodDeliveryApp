f98c5a696c3f45b68e631ce06e980809f82bf7be
Murali <muralimk@outlook.com>
2022-08-25 15:38:45 +0530

    mongodb connect with api

---
+++ FoodieApi/Controllers/FoodieController.cs
@@ -0,0 +1,22 @@
+﻿using FoodieApi.Model;
+using FoodieApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodieApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodieController : ControllerBase
+    {
+        private readonly FoodServices _foodservices;
+        public FoodieController(FoodServices foodServices)
+        {
+            this._foodservices = foodServices;
+        }
+
+        [HttpGet]
+        public async Task<List<Foods>> Get() =>
+            await _foodservices.GetAsync();
+    }
+}
---
+++ FoodieApi/Services/FoodServices.cs
@@ -0,0 +1,23 @@
+﻿using FoodieApi.Model;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace FoodieApi.Services
+{
+    public class FoodServices
+    {
+        private readonly IMongoCollection<Foods> foodcollection;
+        public FoodServices(IOptions<DBSettings> dbSettings)
+        {
+            var foodConnection = new MongoClient(dbSettings.Value.ConnectionString);
+            var foodb = foodConnection.GetDatabase(dbSettings.Value.DatabaseName);
+            foodcollection = foodb.GetCollection<Foods>(dbSettings.Value.CollectionName);
+        }
+
+        public async Task<List<Foods>> GetAsync()
+            => await foodcollection.Find(_ => true).ToListAsync();
+
+
+
+    }
+}
